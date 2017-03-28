d21ecbcc9c8d03571e85f53634faa7511c1ed564
yutaka kobayashi <yutaka.kobayashi@todai-dream-net.com>
2017-03-28 13:42:52 +0000

    add user microposts feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -64,13 +65,13 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
   
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  # def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger] = "Please log in."
+  #     redirect_to login_url
+  #   end
+  # end
   
   def correct_user
     @user = User.find(params[:id])
